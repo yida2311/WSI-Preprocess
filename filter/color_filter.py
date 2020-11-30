1605df371cb6c0d54f0749870c3b28ef78cc8995
yida2311 <ldayi96@163.com>
2020-11-30 16:46:15 +0800

    filter

--- filter/color_filter.py
+++ filter/color_filter.py
@@ -1,4 +1,6 @@
 import os
+import cv2
+import numpy as np
 
 from util import Time, open_image, open_image_np, np_to_pil, mask_rgb, mask_percent, mask_percentage_text, display_img
 import basic_filter as filter
@@ -6,51 +8,37 @@ import basic_filter as filter
 
 def color_filter(np_img, item, save_dir=None, save=False, display=False, hole_size=1000, object_size=600):
     rgb = np_img
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    save_display(rgb, save_dir, item, save, False, 1, 'Original', 'rgb')
-
     mask_not_green = filter.filter_green_channel(rgb)
     rgb_not_green = mask_rgb(rgb, mask_not_green)
-    save_display(rgb_not_green, save_dir, item, save, False, 2, 'Not Green', 'rgb-not-green')
 
     mask_not_gray = filter.filter_grays(rgb, tolerance=13)
     rgb_not_gray = mask_rgb(rgb, mask_not_gray)
-    save_display(rgb_not_gray, save_dir, item, save, False, 3, 'Not Gray', 'rgb-not-gray')
 
     mask_not_red_pen = filter.filter_red_pen(rgb)
     rgb_not_red_pen = mask_rgb(rgb, mask_not_red_pen)
-    save_display(rgb_not_red_pen, save_dir, item, save, False, 4, 'Not Red Pen', 'rgb-not-red-pen')
 
     mask_not_green_pen = filter.filter_green_pen(rgb)
     rgb_not_green_pen = mask_rgb(rgb, mask_not_green_pen)
-    save_display(rgb_not_green_pen, save_dir, item, save, False, 5, 'Not Green Pen', 'rgb-not-green-pen')
 
     mask_not_blue_pen = filter.filter_blue_pen(rgb)
     rgb_not_blue_pen = mask_rgb(rgb, mask_not_blue_pen)
-    save_display(rgb_not_blue_pen, save_dir, item, save, False, 6, 'Not Blue Pen', 'rgb-not-blue-pen')
-
 
     mask_gray_green_pens = mask_not_gray & mask_not_green & mask_not_red_pen & mask_not_green_pen & mask_not_blue_pen
     rgb_gray_green_pens = mask_rgb(rgb, mask_gray_green_pens)
-    save_display(rgb_gray_green_pens, save_dir, item, save, False, 7, "Not Gray, Not Green, No Pens",
-               "rgb-no-gray-no-green-no-pens")
     
     mask_remove_holes = filter.filter_remove_small_holes(mask_not_gray , min_size=hole_size, output_type="bool")
     rgb_remove_holes = mask_rgb(rgb, mask_remove_holes)
-    save_display(rgb_remove_holes, save_dir, item, save, False, 8, 
-                "Not Gray, Not Green, No Pens, \n Remove Small Holes",
-               "rgb-not-green-not-gray-no-pens-remove-small-holes")
 
     mask_remove_obejcts = filter.filter_remove_small_objects(mask_remove_holes, min_size=object_size, output_type="bool")
     rgb_remove_obejcts = mask_rgb(rgb, mask_remove_obejcts)
-    save_display(rgb_remove_obejcts, save_dir, item, save, False, 9, 
-                "Not Gray, Not Green, No Pens, \n Remove Small Objects",
-               "rgb-not-green-not-gray-no-pens-remove-small-objects")
     
-    img = rgb_remove_obejcts
+    n, label, stats, centrids = cv2.connectedComponentsWithStats(np.array(mask_remove_obejcts, dtype='uint8'), connectivity=8)
+    index = np.argmax(stats[1:, 4])+1
+    mask_remove_isolated = np.zeros_like(mask_remove_obejcts, dtype='bool')
+    mask_remove_isolated[label==index] = 1
+    rgb_remove_isolated = mask_rgb(rgb, mask_remove_isolated)
 
-    return img, mask_remove_obejcts, rgb_remove_holes, mask_remove_holes
+    return rgb_remove_isolated, mask_remove_isolated, rgb_remove_obejcts, mask_remove_obejcts
 
 
 def save_display(np_img, dir, item, save, display, filter_num, display_text, file_text, 
--- filter/filters_apply.py
+++ filter/filters_apply.py
@@ -1,16 +1,55 @@
 import os
 from tqdm import tqdm 
+from PIL import Image
 
-from util import Time
-from color_filter import color_filter, open_image_np, np_to_pil
+from util import Time, open_image, pil_to_np_rgb
+from color_filter import color_filter
 
-ROOT = '/media/ldy/7E1CA94545711AE6/OSCC/filter_x8/'
+ROOT = '/media/ldy/7E1CA94545711AE6/OSCC_test/filter_5x/'
 FILTER_ING_DIR = ROOT + 'filtering'
 FILTER_DIR = ROOT + 'filtered_png'
 FILTER_MASK_DIR = ROOT + 'filtered_mask'
-FILTER_NH_DIR = ROOT + 'filtered_nh_png'
-FILTER_MASK_NH_DIR = ROOT + 'filtered_mask_nh'
+FILTER_CMP_DIR = ROOT + 'filtered_cmp_png'
+FILTER_MASK_CMP_DIR = ROOT + 'filtered_mask_cmp'
 
+
+def open_pad_image_np(filename):
+    """
+    Open an image (*.jpg, *.png, etc) and pad it as an RGB NumPy array.
+
+    Args:
+        filename: Name of the image file.
+
+    returns:
+        A NumPy representing an RGB image.
+    """
+    pil_img = open_image(filename)
+    w, h = pil_img.size
+    img_padded = Image.new('RGB', (w+10, h+10), color=(255, 255, 255))
+    img_padded.paste(pil_img, (5, 5, w+5, h+5))
+    np_img = pil_to_np_rgb(img_padded)
+    return np_img
+
+
+def np_to_pil_crop(np_img):
+    """
+    Convert a NumPy array to a PIL Image.
+
+    Args:
+        np_img: The image represented as a NumPy array.
+
+    Returns:
+        The NumPy array converted to a PIL Image.
+    """
+    if np_img.dtype == "bool":
+        np_img = np_img.astype("uint8") * 255
+    elif np_img.dtype == "float64":
+        np_img = (np_img * 255).astype("uint8")
+    pil_img = Image.fromarray(np_img)
+    w, h = pil_img.size
+    pil_img = pil_img.crop(box=(5, 5, w-5, h-5))
+    
+    return pil_img
 
 def apply_color_filter_to_dir(dir, save=True, display=False, hole_size=1000, object_size=600):
     t = Time()
@@ -33,9 +72,9 @@ def apply_color_filter_to_image(item, dir, save=True, display=False, hole_size=1
     print('Processing slide:  {}'.format(item))
     
     image_path = os.path.join(dir, item)
-    np_orig = open_image_np(image_path)
+    np_orig = open_pad_image_np(image_path)
     item = item.split('.')[0]
-    filtered_np_img, filtered_mask, filter_np_img_noholes, filtered_mask_noholes = color_filter(np_orig, item, save_dir=FILTER_ING_DIR, save=False, display=display, hole_size=hole_size, object_size=object_size)
+    filtered_np_img, filtered_mask, filter_np_img_cmp, filtered_mask_cmp = color_filter(np_orig, item, save_dir=FILTER_ING_DIR, save=False, display=display, hole_size=hole_size, object_size=object_size)
     
     save_suffix = '.png'
     item = item + save_suffix
@@ -44,38 +83,37 @@ def apply_color_filter_to_image(item, dir, save=True, display=False, hole_size=1
             os.makedirs(FILTER_DIR)
         if not os.path.isdir(FILTER_MASK_DIR):
             os.makedirs(FILTER_MASK_DIR)
-        if not os.path.isdir(FILTER_NH_DIR):
-            os.makedirs(FILTER_NH_DIR)
-        if not os.path.isdir(FILTER_MASK_NH_DIR):
-            os.makedirs(FILTER_MASK_NH_DIR)
+        if not os.path.isdir(FILTER_CMP_DIR):
+            os.makedirs(FILTER_CMP_DIR)
+        if not os.path.isdir(FILTER_MASK_CMP_DIR):
+            os.makedirs(FILTER_MASK_CMP_DIR)
 
         t1 = Time()
         filter_path = os.path.join(FILTER_DIR, item)
-        pil_img = np_to_pil(filtered_np_img)
+        pil_img = np_to_pil_crop(filtered_np_img)
         pil_img.save(filter_path)
         print("%-20s | Time: %-14s  Name: %s" % ("Save Filtered", str(t1.elapsed()), filter_path))
 
         t1 = Time()
         mask_path = os.path.join(FILTER_MASK_DIR, item)
-        pil_mask = np_to_pil(filtered_mask)
+        pil_mask = np_to_pil_crop(filtered_mask)
         pil_mask.save(mask_path)
         print("%-20s | Time: %-14s  Name: %s" % ("Save Mask", str(t1.elapsed()), mask_path))
 
         t1 = Time()
-        filter_nh_path = os.path.join(FILTER_NH_DIR, item)
-        pil_img_nh = np_to_pil(filter_np_img_noholes)
-        pil_img_nh.save(filter_nh_path)
-        print("%-20s | Time: %-14s  Name: %s" % ("Save Filtered NoHoles", str(t1.elapsed()), filter_nh_path))
+        filter_cmp_path = os.path.join(FILTER_CMP_DIR, item)
+        pil_img_cmp = np_to_pil_crop(filter_np_img_cmp)
+        pil_img_cmp.save(filter_cmp_path)
+        print("%-20s | Time: %-14s  Name: %s" % ("Save Filtered CMP", str(t1.elapsed()), filter_cmp_path))
 
         t1 = Time()
-        mask_nh_path = os.path.join(FILTER_MASK_NH_DIR, item)
-        pil_mask_nh = np_to_pil(filtered_mask_noholes)
-        pil_mask_nh.save(mask_nh_path)
-        print("%-20s | Time: %-14s  Name: %s" % ("Save Mask NoHoles", str(t1.elapsed()), mask_nh_path))
-
+        mask_cmp_path = os.path.join(FILTER_MASK_CMP_DIR, item)
+        pil_mask_cmp = np_to_pil_crop(filtered_mask_cmp)
+        pil_mask_cmp.save(mask_cmp_path)
+        print("%-20s | Time: %-14s  Name: %s" % ("Save Mask CMP", str(t1.elapsed()), mask_cmp_path))
 
 if __name__ == '__main__':
-    src_dir = '/media/ldy/7E1CA94545711AE6/OSCC/scaledown8_png/'
-    hole_size = 2000
-    object_size = 3000
+    src_dir = '/media/ldy/7E1CA94545711AE6/OSCC_test/5x_png/'
+    hole_size = 2000*3000
+    object_size = 12000
     apply_color_filter_to_dir(src_dir, hole_size=hole_size, object_size=object_size)
